284ed58001013f6b30be8660e380537448ef5e63
Kelvin Yesutor <franciseshunboateng@gmail.com>
2019-12-03 15:45:55 +0000

    Added hi

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       onGenerateRoute: Router.generateRoutes,
-      initialRoute: '/',
+      initialRoute: '/hi',
     );
   }
 }
